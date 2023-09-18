534286dc95c209344d21092c63202a3d26a28789
Bahram Rahmani <bahramrahmani1369@gmail.com>
2023-09-18 09:53:17 +0330

    Add files via upload

---
+++ scripts/embassy.ts
@@ -0,0 +1,6 @@
+export { properties } from "./procedures/properties.ts";
+export { setConfig } from "./procedures/setConfig.ts";
+export { getConfig } from "./procedures/getConfig.ts";
+export { migration } from "./procedures/migrations.ts";
+export { health } from "./procedures/health.ts";
+export { main } from "./procedures/main.ts";
